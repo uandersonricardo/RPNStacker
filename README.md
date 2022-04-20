908d7787fcda985382b93ef038377088289b70c2
Uanderson Ricardo <uandersonrfs@gmail.com>
2022-04-20 15:45:23 -0300

    implemented scanning and tokens

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # RPNStacker
 
 ## Descrição
-Task 01 de Compiladores 2021.2 EC
+Tasks de Compiladores 2021.2 EC
 
-## Roteiro
+## Task 01
 - Rever a Aula 07 sobre notacao pós-fixa [_reverse polish notation_]
 - Implementar uma linguagem RPN stacker em Java usando uma pilha como estrutura de dados
 - Programa lê um arquivo com a expressao em RPN e avalia
+
 
 Exemplo de entrada:
 ```
@@ -14,6 +15,37 @@ Exemplo de entrada:
 10
 +
 Saida: 20
+``` 
+
+## Task 02
+- Rever a Aula 11 sobre introdução à análise léxica [_scanning_]
+- Evoluir o projeto da Task 01 para implementar uma _feature_ de _scanning_:
+   - No geral, nosso programa lê um arquivo com a expressao em RPN e devolve a expressão avaliada;
+   - A _feature_ de _scanning_ deve retornar uma lista de _tokens_;
+   - A partir dessa lista de _tokens_ que é realizada a interpretacao das expressões com uma pilha;
+   - A _feature_ de _scanning_ deve retornar um erro caso não reconheça um "num" [número] ou "op" [operador].
+
+Exemplo de entrada [com sucesso]:
+``` 
+10
+10
++
+
+// a lista de tokens reconhecida [caso a imprima]
+Token [type=NUM, lexeme=10]
+Token [type=NUM, lexeme=10]
+Token [type=PLUS, lexeme=+]
+
+Saida: 20
+``` 
+
+Exemplo de entrada [com falha]:
+``` 
+10
+s
++
+
+Error: Unexpected character: s
 ``` 
 
 ## Aluno
--- RPNStacker.java
+++ RPNStacker.java
@@ -1,53 +1,95 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
 
+import Token;
+import TokenType;
+
 public class RPNStacker {
     public static void main(String[] args) {
-        File file = new File("Calc1.stk");
-
         try {
-            Scanner scan = new Scanner(file);
+            List<Token> tokens = scan("Calc1.stk");
             Stack<String> stack = new Stack<String>();
-    
-            while (scan.hasNextLine()) {
-                String token = scan.nextLine();
 
-                if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+            while (!tokens.isEmpty()) {
+                Token token = tokens.remove(0);
+
+                System.out.println(token);
+
+                if (token.type != TokenType.NUM) {
                     String op1 = stack.pop();
                     String op2 = stack.pop();
 
                     int result = 0;
                     
-                    switch (token) {
-                        case "+":
+                    switch (token.type) {
+                        case PLUS:
                             result = Integer.parseInt(op2) + Integer.parseInt(op1);
                             break;
-                        case "-":
+                        case MINUS:
                             result = Integer.parseInt(op2) - Integer.parseInt(op1);
                             break;
-                        case "*":
+                        case STAR:
                             result = Integer.parseInt(op2) * Integer.parseInt(op1);
                             break;
-                        case "/":
+                        case SLASH:
                             result = Integer.parseInt(op2) / Integer.parseInt(op1);
+                            break;
+                        default:
                             break;
                     }
 
                     stack.push(String.valueOf(result));
                 } else {
-                    stack.push(token);
+                    stack.push(token.lexeme);
                 }
             }
 
             System.out.println(stack.pop());
-
-            scan.close();
         } catch (FileNotFoundException e) {
             System.out.println("Arquivo não encontrado");
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
         } catch (Exception e) {
             System.out.println("Não foi possível calcular a operação");
         }
+    }
+
+    private static List<Token> scan(String filename) throws FileNotFoundException {
+        File file = new File(filename);
+
+        Scanner scan = new Scanner(file);
+        List<Token> tokens = new ArrayList<Token>();
+
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine().trim();
+
+            Token token;
+
+            if (line.equals("+")) {
+                token = new Token(TokenType.PLUS, line);
+            } else if (line.equals("-")) {
+                token = new Token(TokenType.MINUS, line);
+            } else if (line.equals("*")) {
+                token = new Token(TokenType.STAR, line);
+            } else if (line.equals("/")) {
+                token = new Token(TokenType.SLASH, line);
+            } else if (line.matches("[0-9]+")) {
+                token = new Token(TokenType.NUM, line);
+            } else {
+                scan.close();
+
+                throw new RuntimeException("Token inválido: " + line);
+            }
+            
+            tokens.add(token);
+        }
+
+        scan.close();
+
+        return tokens;
     }
 }
